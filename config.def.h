7d3bc7446f431324e80c9cfa2c492027062fad12
maphapthieunu <npl@duck.com>
2024-12-23 11:41:43 +0700

    patches/st-scrollback-mouse-altscreen-20220127-2c5edf2.diff

--- config.def.h
+++ config.def.h
@@ -201,8 +201,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},              0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},              0, /* !alt */ -1 },
 };
 
 /*
